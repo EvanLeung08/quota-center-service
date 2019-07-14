bd061581f104e517866e440accf26bff6e6dd0c4
Evan43789596 <10856214@163.com>
2019-07-14 20:16:30 +0800

    feat(#All):initialize quota-center-service project

---
+++ src/main/java/com/evanshare/quota/QuotaCenterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.evanshare.quota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotaCenterServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuotaCenterServiceApplication.class, args);
+  }
+
+}
